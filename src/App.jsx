e6c339461a03658e3cbcd0ed168406111796864f
Priyanshu-choudhary <broyadi23@gmail.com>
2024-08-05 23:32:54 +0530

    navbr in TF

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-
-import HomePage from './Home/HomePage'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './Home/HomePage';
+import ShopPage from './Shop/ShopPage';
+import AboutPage from './About/AboutPage';
+import ServicesPage from './Services/ServicesPage';
+import BlogPage from './Blog/BlogPage';
+import ContactPage from './Contact/ContactPage';
+import NotFoundPage from './NotFoundPage'; // Import the NotFoundPage component
+import NavBar from './NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-   <HomePage/>
-    </>
-  )
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} /> {/* Add this route */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/Home/NavBar.jsx
+++ src/Home/NavBar.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <nav className="bg-gray-800 text-white p-4">
       <div className="container mx-auto flex items-center justify-between">
-        <a className="text-white text-2xl font-bold" href="index.html">
+        <Link className="text-white text-2xl font-bold" to="/">
           TechnoFarm<span>.</span>
-        </a>
+        </Link>
         <button className="text-white md:hidden" aria-label="Toggle navigation">
           <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
           </svg>
         </button>
         <div className="hidden md:flex md:items-center md:space-x-4">
-          <a className="hover:text-gray-400" href="index.html">Home</a>
-          <a className="hover:text-gray-400" href="shop.html">Shop</a>
-          <a className="hover:text-gray-400" href="about.html">About us</a>
-          <a className="hover:text-gray-400" href="services.html">Services</a>
-          <a className="hover:text-gray-400" href="blog.html">Blog</a>
-          <a className="hover:text-gray-400" href="contact.html">Contact us</a>
+          <Link className="hover:text-gray-400" to="/">Home</Link>
+          <Link className="hover:text-gray-400" to="/shop">Shop</Link>
+          <Link className="hover:text-gray-400" to="/about">About us</Link>
+          <Link className="hover:text-gray-400" to="/services">Services</Link>
+          <Link className="hover:text-gray-400" to="/blog">Blog</Link>
+          <Link className="hover:text-gray-400" to="/contact">Contact us</Link>
         </div>
         <div className="hidden md:flex md:space-x-4">
-          <a className="hover:text-gray-400" href="#"><img src="images/user.svg" alt="User" /></a>
-          <a className="hover:text-gray-400" href="cart.html"><img src="images/cart.svg" alt="Cart" /></a>
+          <Link className="hover:text-gray-400" to="#"><img src="images/user.svg" alt="User" /></Link>
+          <Link className="hover:text-gray-400" to="/cart"><img src="images/cart.svg" alt="Cart" /></Link>
         </div>
       </div>
     </nav>
